4166800fc4197791de3ebe0bdfc06cde6fbc313c
Steven <41983313+3steve3@users.noreply.github.com>
2021-12-25 10:55:24 -0700

    Update Nvidia Screen Tear Fix.sh

--- Nvidia Screen Tear Fix.sh
+++ Nvidia Screen Tear Fix.sh
@@ -8,6 +8,5 @@ sudo eopkg remove xorg-driver-video-nouveau
 sudo mkdir -p /etc/kernel/cmdline.d
 sudo echo "nvidia-drm.modeset=1" | sudo tee /etc/kernel/cmdline.d/50-nvidia-drm.conf
 sudo clr-boot-manager update
-sudo usysconf run -f
 sudo reboot
 fi
