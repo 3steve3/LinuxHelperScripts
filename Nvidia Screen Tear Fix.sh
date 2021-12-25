e42b1b55bebeee4964e9f4723b3eaac26ab93e72
Steven <41983313+3steve3@users.noreply.github.com>
2021-12-25 09:56:25 -0700

    Add files via upload

---
+++ Nvidia Screen Tear Fix.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+if [[ "$EUID" != 0 ]]; then
+	echo "Run as root"
+	exit
+else
+sudo eopkg install nvidia-glx-driver-common nvidia-glx-driver-current nvidia-glx-driver-modaliases nvidia-glx-driver-32bit
+sudo eopkg remove xorg-driver-video-nouveau
+sudo mkdir -p /etc/kernel/cmdline.d
+sudo echo "nvidia-drm.modeset=1" | sudo tee /etc/kernel/cmdline.d/50-nvidia-drm.conf
+sudo clr-boot-manager update
+fi
