79d1f7ee98fdf1bd79d554215134df5efbc5cd31
Steven <41983313+3steve3@users.noreply.github.com>
2022-03-18 23:17:39 -0600

    Create NewInstall.sh

---
+++ Fedora/NewInstall.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+sudo dnf update
+sudo rpm -Uvh http://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm
+sudo rpm -Uvh http://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
